cb8d87da9a05c1c8347536d5bb5ff059647d30e2
kadet <techkadet@gmail.com>
2021-08-24 03:01:38 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import url("http://fonts.cdnfonts.com/css/monument-extended");
+
+* {
+  font-family: "Monument Extended", sans-serif;
+  box-sizing: border-box;
+  scroll-behavior: smooth;
+
+  --primary: #402a85;
+  --primary-100: #45108a;
+  --primary-200: #352058;
+  --secondary: #43e8e1;
+  --tetiary: #ffa98c;
+  --black: #130217;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  color: var(--black);
 }
 </style>
--- src/common/Header.vue
+++ src/common/Header.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
+  <div>
+    <h1>Header</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
+  name: "Header",
 };
 </script>
